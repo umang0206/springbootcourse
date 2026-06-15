69f893613342d260e754cadcc4d62bc9713b1384
umang0206 <umang.prayas84jee19@gmail.com>
2026-06-15 23:29:50 +0530

    module 2.4 Service layer, model mapper explained

---
+++ restapi/src/main/java/com/springbootwebserver/restapi/service/EmployeeService.java
@@ -0,0 +1,43 @@
+package com.springbootwebserver.restapi.service;
+
+import com.springbootwebserver.restapi.dto.EmployeeDTO;
+import com.springbootwebserver.restapi.entity.EmployeeEntity;
+import com.springbootwebserver.restapi.repository.EmployeeRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+    private final ModelMapper modelMapper;
+
+    public EmployeeService(EmployeeRepository employeeRepository, ModelMapper modelMapper) {
+        this.employeeRepository = employeeRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public EmployeeDTO findById(Long employeeId) {
+        EmployeeEntity employeeEntity = employeeRepository.findById(employeeId).orElse(null);
+        return modelMapper.map(employeeEntity, EmployeeDTO.class);
+    }
+
+    public List<EmployeeDTO> getAllEmployee() {
+        List<EmployeeEntity> employeeEntities = employeeRepository.findAll();
+        return employeeEntities
+                .stream()
+                .map(employeeEntity -> modelMapper.map(employeeEntity,EmployeeDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    public EmployeeDTO save(EmployeeDTO inputEmployee) {
+        EmployeeEntity toSaveEntity = modelMapper.map(inputEmployee, EmployeeEntity.class);
+        EmployeeEntity employeeEntity = employeeRepository.save(toSaveEntity);
+        return modelMapper.map(employeeEntity, EmployeeDTO.class);
+
+    }
+}
