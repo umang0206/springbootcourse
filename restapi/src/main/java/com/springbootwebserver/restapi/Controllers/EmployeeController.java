86737455b6fb0dd80aad7f0fd420e725af4457fc
umang0206 <umang.prayas84jee19@gmail.com>
2026-06-15 21:27:48 +0530

    Module 2.2 Persistance layer, JPA repository and H2 mem database

--- restapi/src/main/java/com/springbootwebserver/restapi/Controllers/EmployeeController.java
+++ restapi/src/main/java/com/springbootwebserver/restapi/Controllers/EmployeeController.java
@@ -1,9 +1,12 @@
 package com.springbootwebserver.restapi.Controllers;
 
 import com.springbootwebserver.restapi.dto.EmployeeDTO;
+import com.springbootwebserver.restapi.entity.EmployeeEntity;
+import com.springbootwebserver.restapi.repository.EmployeeRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
@@ -13,21 +16,29 @@ public class EmployeeController {
 //    public String getMySecretMessage(){
 //        return "secret data is: 427569$^&^%$#$";
 //    }
+    final EmployeeRepository employeeRepository;
+
+    public EmployeeController (EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 
     @GetMapping("/{employeeId}")
-    public EmployeeDTO getEmployeeById(@PathVariable Long employeeId){
-        return new EmployeeDTO(employeeId,"Umang Kumar", "umang@gmail.com", 25, LocalDate.of(2024,03, 20),true);
+    public EmployeeEntity getEmployeeById(@PathVariable Long employeeId){
+//        return new EmployeeDTO(employeeId,"Umang Kumar", "umang@gmail.com", 25, LocalDate.of(2024,03, 20),true);
+        return employeeRepository.findById(employeeId).orElse(null);
     }
 
     @GetMapping
-    public String getAllEmployee(@RequestParam Integer age, @RequestParam(required = false) String sortBy){  //in requestParam if mark required as false it so if user willl not pass that field in request still it will work
-        return "Hi My age is " + age + " sort by " + sortBy;
+    public List<EmployeeEntity> getAllEmployee(@RequestParam(required = false) Integer age, @RequestParam(required = false) String sortBy){  //in requestParam if mark required as false it so if user willl not pass that field in request still it will work
+//        return "Hi My age is " + age + " sort by " + sortBy;
+
+        return employeeRepository.findAll();
     }
 
     @PostMapping
-    public EmployeeDTO createEmployee(@RequestBody EmployeeDTO inputEmployee){
-        inputEmployee.setId(101L);
-        return inputEmployee;
+    public EmployeeEntity createEmployee(@RequestBody EmployeeEntity inputEmployee){
+//        inputEmployee.setId(101L);
+        return employeeRepository.save(inputEmployee);
     }
 
     @PutMapping
---
+++ restapi/src/main/java/com/springbootwebserver/restapi/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.springbootwebserver.restapi.repository;
+
+import com.springbootwebserver.restapi.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+}
