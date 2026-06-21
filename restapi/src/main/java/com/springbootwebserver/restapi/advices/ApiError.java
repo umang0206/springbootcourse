83e4ed3f565a68b5c28226c63d68a155141e18a0
umang0206 <umang.prayas84jee19@gmail.com>
2026-06-22 00:43:19 +0530

    module 2.8 Transforming API response in spring MVC

--- restapi/src/main/java/com/springbootwebserver/restapi/advices/ApiError.java
+++ restapi/src/main/java/com/springbootwebserver/restapi/advices/ApiError.java
@@ -2,15 +2,24 @@ package com.springbootwebserver.restapi.advices;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 @Data
 @Builder
+@Getter
+@Setter
 public class ApiError {
     private HttpStatus status;
     private String message;
     private List<String> subErrors;
+
+    public HttpStatusCode getStatus() {
+        return status;
+    }
 }
